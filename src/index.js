a2fdeffdc62c238a93e46a1f1b458c9e282a614a
zbin66666 <binz6@uci.edu>
2023-10-04 20:44:44 -0700

    finised

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {Provider} from 'react-redux'
+
 
 import './api/server'
+import store from './store'
+import { fetchTodos } from './features/todos/todosSlice'
 
-ReactDOM.render(
-  <React.StrictMode>
+store.dispatch(fetchTodos())
+
+  ReactDOM.render(
+    <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    </Provider>
+    </React.StrictMode>,
+document.getElementById('root'))
